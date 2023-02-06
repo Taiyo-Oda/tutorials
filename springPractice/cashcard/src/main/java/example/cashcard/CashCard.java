6baab628e686fa27bbd2ec6f10bef40dc38b4d5c
小田太陽 <noda0630@gmail.com>
2023-02-07 07:37:31 +0900

    Spring Security を使用して、認証済みで許可されたユーザーだけが Family Cash Card API にアクセス可能に修正

--- springPractice/cashcard/src/main/java/example/cashcard/CashCard.java
+++ springPractice/cashcard/src/main/java/example/cashcard/CashCard.java
@@ -3,5 +3,5 @@ package example.cashcard;
 import org.springframework.data.annotation.Id;
 
 // @id : CashCardRepositoryの@Idとしてidを設定します。
-public record CashCard(@Id Long id, Double amount) {
+public record CashCard(@Id Long id, Double amount, String owner) {
 }
