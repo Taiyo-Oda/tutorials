9f6923a8d26f1ee8025cd5c8667cce876f2274c1
小田太陽 <noda0630@gmail.com>
2023-03-02 08:42:08 +0900

    アプリケーションのBoot化を実施

--- springPractice/lab/32-jdbc-autoconfig/src/test/java/rewards/SystemTestConfig.java
+++ springPractice/lab/32-jdbc-autoconfig/src/test/java/rewards/SystemTestConfig.java
@@ -1,14 +1,11 @@
 package rewards;
 
-import config.RewardsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
-import javax.sql.DataSource;
+import config.RewardsConfig;
 
 /**
  * Sets up an embedded in-memory HSQL database, primarily for testing.
@@ -24,15 +21,4 @@ public class SystemTestConfig {
 	// - Comment out the @Bean method below so this method
 	//   is no longer called
 
-	@Bean
-	public DataSource dataSource() {
-		logger.debug("Creating the datasource bean explicitly");
-
-		return
-			(new EmbeddedDatabaseBuilder())
-			.addScript("classpath:rewards/testdb/schema.sql")
-			.addScript("classpath:rewards/testdb/data.sql")
-			.build();
-	}
-	
 }
